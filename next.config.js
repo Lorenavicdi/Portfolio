7a906a3a08e1f7721fbba37ed353cb2c5d3458a1
Ralvarez <R.alvgar@hotmail.com>
2021-01-30 13:43:19 +0100

    new try

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/Lorenavicdi/portfolio/tree/gh-pages/"
+    ? "https://cdn.statically.io/gh/Lorenavicdi/portfolio/tree/gh-pages/index.js"
     : "",
 };
